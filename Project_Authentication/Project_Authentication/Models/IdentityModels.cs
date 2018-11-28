a5ac324b88cf28dad3760fa38e40dccf93fda8ef
fashraf186 <fashraf@xavor.com>
2018-11-28 16:36:47 +0500

    Change tha name of the database in the identitymodel class. also in the web.config

--- Project_Authentication/Project_Authentication/Models/IdentityModels.cs
+++ Project_Authentication/Project_Authentication/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Project_Authentication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Login_Data", throwIfV1Schema: false)
         {
         }
 
